af162c7626c606597fa9581e65bf9c370570b134
hyojin52 <khj960502@naver.com>
2024-05-31 16:19:06 +0900

    first commit

---
+++ src/main/java/com/example/roadview/RoadviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.roadview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadviewApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(RoadviewApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/com/example/roadview/RoadviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roadview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadviewApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
